65423c7795065e51b698d95a3bc3f4441a026155
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-09-05 12:52:48 -0500

    updated comments.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
-import sys
 from flask import render_template
-from flask_flatpages import pygments_style_defs
 from app import app, flatpages, freezer
 
 POST_DIR = app.config['POST_DIR']
+
+########################################
+############  App URLs  ################
+########################################
+
 @app.route("/")
 def index():
 	"""
@@ -19,17 +22,6 @@ def tag(tag):
 	"""
 	posts = [p for p in flatpages if tag in p.meta.get('tags', [])]
 	return render_template('tag.html', pages=posts, tag=tag)
-
-@freezer.register_generator
-def tag():
-  """
-	This generator will generator all the static pages 
-	needed for the any /tag/ Urls
-	"""
-	all_tags = [p.meta.get('tags',[]) for p in flatpages]
-	unique_tags = set(sum(all_tags, []))
-	for tag in unique_tags:
-		yield 'tag', {'tag': tag}
 
 @app.route("/blog/")
 def posts():
@@ -48,3 +40,20 @@ def post(name):
 	path = '{}/{}'.format(POST_DIR, name)
 	post = flatpages.get_or_404(path)
 	return render_template('post.html', post=post)
+
+
+########################################
+######  Flask-Frozen Generators   ######
+########################################
+
+@freezer.register_generator
+def tag():
+  """
+  This generator will generator all the static pages 
+  needed for the any /tag/ Urls
+  """
+  #Grabs all the tags. Flatten them into a set.
+  all_tags = [p.meta.get('tags',[]) for p in flatpages] 
+  unique_tags = set(sum(all_tags, []))
+  for tag in unique_tags:
+    yield 'tag', {'tag': tag}
