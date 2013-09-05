cf5740aba79d769f32ad577100f6f81e15b1b17b
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-09-04 21:23:08 -0500

    update

--- README.md
+++
@@ -1,6 +0,0 @@
-dustinshahidehpour.com
-======================
-
-A static website built with Flask, and AWS Web Services
-
-Repo for dustinshahidehpour.com
--- run.py
+++
@@ -1,8 +0,0 @@
-import sys
-from app import app, freezer 
-
-if __name__ == "__main__":
-	if len(sys.argv) > 1 and sys.argv[1] == "build":
-		freezer.freeze()
-	else:
-		app.run(host='0.0.0.0', debug=True)
