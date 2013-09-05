787256efd4da0db1a65be07944b7fa181714d7af
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-09-05 10:33:38 -0500

    Added comments to views.py

--- app/views.py
+++ app/views.py
@@ -6,22 +6,34 @@ from app import app, flatpages
 POST_DIR = app.config['POST_DIR']
 @app.route("/")
 def index():
+	"""
+	This method returns the main page of the site.
+	"""
 	return "Hello World"
-
 
 @app.route("/tag/<string:tag>/")
 def tag(tag):
+	"""
+	This method grabs all the posts with a specific tag,
+	and renders a new page with all of them listed.
+	"""
 	posts = [p for p in flatpages if tag in p.meta.get('tags', [])]
 	return render_template('tag.html', pages=posts, tag=tag)
 
 @app.route("/blog/")
 def posts():
+	"""
+	This method returns a page with a list of every post.
+	"""
 	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
 	posts.sort(key=lambda item:item['date'], reverse=False)
 	return render_template('posts.html', posts=posts)
 
 @app.route('/blog/<name>/')
 def post(name):
+	"""
+	This method returns a page containing a single blog entry.
+	"""
 	path = '{}/{}'.format(POST_DIR, name)
 	post = flatpages.get_or_404(path)
 	return render_template('post.html', post=post)
