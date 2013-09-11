583b7b32d0baf6c23d2cf773b86e04e8fe56dc10
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-09-11 16:39:25 -0500

    Added new colors, changed Github band, continued with reformatting of pages.

--- app/views.py
+++ app/views.py
@@ -7,14 +7,16 @@ POST_DIR = app.config['POST_DIR']
 ############  App URLs  ################
 ########################################
 
+
 @app.route("/")
 def index():
     """
     This method returns the main page of the site.
     """
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-    posts.sort(key=lambda item:item['date'], reverse=True)
+    posts.sort(key=lambda item: item['date'], reverse=True)
     return render_template('index.html', posts=posts[:4])
+
 
 @app.route("/tag/<string:tag>/")
 def tag(tag):
@@ -25,24 +27,27 @@ def tag(tag):
     posts = [p for p in flatpages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=posts, tag=tag)
 
+
 @app.route("/blog/")
 def posts():
     """
     This method returns a page with a list of every post.
     """
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-    posts.sort(key=lambda item:item['date'], reverse=True)
+    posts.sort(key=lambda item: item['date'], reverse=True)
 
     #Get the tag count from all posts.
     tags = sum([p.meta.get('tags', []) for p in flatpages], [])
     tag_count = {}
     for tag in tags:
-      if tag_count.get(tag) is None:
-        tag_count[tag] = 1
-      else:
-        tag_count[tag] += 1
+        if tag_count.get(tag) is None:
+            tag_count[tag] = 1
+        else:
+            tag_count[tag] += 1
 
-    return render_template('posts.html', posts=posts, tags=sorted(tag_count), tag_count=tag_count)
+    return render_template('posts.html', posts=posts,
+                           tags=sorted(tag_count), tag_count=tag_count)
+
 
 @app.route('/blog/<name>/')
 def post(name):
@@ -53,23 +58,33 @@ def post(name):
     post = flatpages.get_or_404(path)
     return render_template('post.html', post=post)
 
+
+@app.route('/contact/')
+def contact():
+    """
+    This method returns all the user's contact information.
+    """
+    return render_template('contact.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 ########################################
 ######  Flask-Frozen Methods    ########
 ########################################
 
+
 @freezer.register_generator
 def tag():
-  """
-  This generator will generator all the static pages 
-  needed for the any /tag/ Urls
-  """
-  #Grabs all the tags. Flatten them into a set.
-  all_tags = [p.meta.get('tags',[]) for p in flatpages] 
-  unique_tags = set(sum(all_tags, []))
-  for tag in unique_tags:
-    yield 'tag', {'tag': tag}
+    """
+    This generator will generator all the static pages
+    needed for the any /tag/ Urls
+    """
+    #Grabs all the tags. Flatten them into a set.
+    all_tags = [p.meta.get('tags', []) for p in flatpages]
+    unique_tags = set(sum(all_tags, []))
+    for tag in unique_tags:
+        yield 'tag', {'tag': tag}
