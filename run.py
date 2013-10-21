f526222d4822310ab75578fda494f1f2fb2bdae9
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-10-20 22:52:15 -0500

    upgraded run.py to always upload new content.

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import sys
+import shutil
 from app import freezer, app
 
 if __name__ == "__main__":
-	if len(sys.argv) > 1 and sys.argv[1] == "build":
-		freezer.freeze()
-	else:
-		app.run(host='0.0.0.0', debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        shutil.rmtree('./app/build')
+        freezer.freeze()
+    else:
+        app.run(host='0.0.0.0', debug=True)
